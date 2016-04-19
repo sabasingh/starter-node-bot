1fa36723b7df013d57d64a74eeb502f3d0fa4505
ixdsaba <Saba Singh>
2016-04-19 00:01:33 -0400

    testing hello 2

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ controller.on('bot_channel_join', function (bot, message) {
 
 controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
   bot.reply(message, 'Hello.')
-  bot.reply(message, 'It\'s nice to talk to you directly.')
+  bot.reply(message, 'It\'s nice to talk to you right now wakakakakakaa.')
 })
 
 controller.hears('.*', ['mention'], function (bot, message) {
