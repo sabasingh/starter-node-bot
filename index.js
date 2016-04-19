336bd4ce85d2057791685489d07104a9999639c3
ixdsaba <Saba Singh>
2016-04-19 03:42:50 -0400

    card

--- index.js
+++ index.js
@@ -53,10 +53,10 @@ controller.hears(['opt in', 'optin'], 'direct_message,direct_mention,mention', f
             {
                 pattern: bot.utterances.yes,
                 callback: function(response, convo) {
-                    //convo.say('Okay, let\'s continue with where we left off. \nThis is what you look like in chats. Edit your profile details by tapping on your image or your name here:');
+                    convo.say('Okay, let\'s continue with where we left off. \nThis is what you look like in chats. Edit your profile details by tapping on your image or your name here:');
                     convo.say({
                         
-                            "text": "Okay, let\'s continue with where we left off. \nThis is what you look like in chats. Edit your profile details by tapping on your image or your name here: http://i.imgur.com/E5i8kwl.png",
+                            "text": "http://bit.ly/1ShRV2g",
                             "attachments": [
                                 {
                                     "text": "Let me know when you are done editing your details!"
@@ -66,7 +66,6 @@ controller.hears(['opt in', 'optin'], 'direct_message,direct_mention,mention', f
                     });
                     //convo.say('let me know when you are done editing your details');
                     convo.say('`I\'m done` `Skip this step`');
-//                    convo.say('`Skip this step`');
                     convo.next();
               }
             },
