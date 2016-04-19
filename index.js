70483ffa3af6612d4a4ef9017e4608c8a125a6df
ixdsaba <Saba Singh>
2016-04-19 00:00:02 -0400

    testing hello

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ controller.on('bot_channel_join', function (bot, message) {
   bot.reply(message, "I'm here!")
 })
 
-controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
-  bot.reply(message, 'Hello.')
-})
+//controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
+//  bot.reply(message, 'Hello.')
+//})
 
 controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
   bot.reply(message, 'Hello.')
