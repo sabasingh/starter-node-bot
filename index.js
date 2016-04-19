cacb55515147db7339f89702075f920331a24c06
ixdsaba <Saba Singh>
2016-04-19 02:46:33 -0400

    initiate

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ controller.on('bot_channel_join', function (bot, message) {
 })
 
 
-bot.say(
+bot.api.chat.postMessage(
   {
     text: 'my message text',
     channel: 'U11NXAJU8' // a valid slack channel, group, mpim, or im ID
